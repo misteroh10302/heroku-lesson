9711e359dd2a1162abce6b8778c5b896f7891c7b
misteroh <madeline@done-digital.com>
2020-06-03 19:25:28 -0700

    updates heroku lesson

---
+++ public/js/App.js
@@ -0,0 +1,12 @@
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> Hello Heroku! </h1>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('.container'));
